95180b1b8d58560fe155ecb9dbe786b1fb69da49
gilberto_junior <gilbertopsantosjr@gmail.com>
2019-12-17 09:08:34 +0000

    adding a simple test

--- demo/src/test/java/com/gilbertosantos/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/gilbertosantos/demo/DemoApplicationTests.java
@@ -1,13 +1,35 @@
 package com.gilbertosantos.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.gilbertosantos.DemoApplication;
+import com.gilbertosantos.dao.FoodService;
+import com.gilbertosantos.model.Food;
+
+
+@SpringBootTest(classes = DemoApplication.class)
+@ActiveProfiles("TEST")
 class DemoApplicationTests {
 
+	@Autowired
+	FoodService foodService;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+
+	@Test
+	void testWhetherFoodIsReturnByToday() {
+		 Optional<Food> findMenuDay = foodService.findMenuDay();
+		 assertNotNull(findMenuDay.get());
 	}
 
 }
